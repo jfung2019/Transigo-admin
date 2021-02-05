e6c38d467264749cb15e4107a7a2130fd06a5183
JasonTsui <jason@plangora.com>
2021-02-05 13:34:48 +0800

    add db host to test

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :transigo_admin, TransigoAdmin.Repo,
   username: "postgres",
   password: "postgres",
   database: "transigo_admin_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
