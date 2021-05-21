58ee719ad26da73c9c496ccb51486d498a4df588
JasonTsui <jason@plangora.com>
2021-05-21 10:37:59 +0800

    fix migration

--- priv/repo/migrations/20201118063855_create_user_table.exs
+++ priv/repo/migrations/20201118063855_create_user_table.exs
@@ -4,7 +4,7 @@ defmodule TransigoAdmin.Repo.Migrations.CreateUserTable do
   def change do
     execute "CREATE EXTENSION IF NOT EXISTS citext", "DROP EXTENSION citext"
 
-    create table(:admins, primary_key: false) do
+    create_if_not_exists table(:admins, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :firstname, :string, null: false
       add :lastname, :string, null: false
@@ -20,7 +20,7 @@ defmodule TransigoAdmin.Repo.Migrations.CreateUserTable do
       timestamps()
     end
 
-    create unique_index(:admins, [:email])
-    create unique_index(:admins, [:username])
+    create_if_not_exists unique_index(:admins, [:email])
+    create_if_not_exists unique_index(:admins, [:username])
   end
 end
