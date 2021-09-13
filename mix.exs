4c8b9d35647901ac8f0fdc7a1ab882602e119a57
gabriel <gabriel@transigo.io>
2021-09-13 16:17:24 +0800

    add puid package

--- mix.exs
+++ mix.exs
@@ -64,7 +64,8 @@ defmodule TransigoAdmin.MixProject do
       {:hackney, "~> 1.17"},
       {:sentry, "~> 8.0"},
       {:briefly, "~> 0.3"},
-      {:nimble_totp, "~> 0.1.2"}
+      {:nimble_totp, "~> 0.1.2"},
+      {:puid, "~> 1.0"}
     ]
   end
 
