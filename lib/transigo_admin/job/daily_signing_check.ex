acd8933093a08431dc9a71510998f80074b1ff40
gabriel <gabriel@transigo.io>
2021-09-14 16:37:31 +0800

    rename update_exporter hx_request

--- lib/transigo_admin/job/daily_signing_check.ex
+++ lib/transigo_admin/job/daily_signing_check.ex
@@ -69,7 +69,7 @@ defmodule TransigoAdmin.Job.DailySigningCheck do
 
     case @hs_api.create_signature_request(payload) do
       {:ok, %{"signature_request" => %{"signature_request_id" => req_id}}} ->
-        Account.update_exporter(exporter, %{
+        Account.update_exporter_hs_request(exporter, %{
           hellosign_signature_request_id: req_id,
           hs_signing_status: "awaiting_signature"
         })
