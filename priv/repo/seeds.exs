aadeb86ee09bdd1390de49b0b540b3519d7b638b
gabriel <gabriel@transigo.io>
2021-09-16 19:47:48 +0800

    seeds for testing

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,8 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias TransigoAdmin.Repo
 
-TransigoAdmin.Repo.insert!(%TransigoAdmin.Credit.Marketplace{
-    origin: "DH",
-    marketplace: "DHGate"
+Repo.insert!(%TransigoAdmin.Credit.Marketplace{
+  origin: "DH",
+  marketplace: "DHGate"
+})
+
+Repo.insert!(%TransigoAdmin.Account.Contact{
+  contact_transigo_uid: TransigoAdmin.DataLayer.generate_uid("con"),
+  first_name: "Bill",
+  last_name: "TestCase",
+  mobile: "7072934628",
+  work_phone: "70793452837",
+  email: "bill@testcase.com",
+  role: "President",
+  country: "US",
+  address: "8842 48th Ave Anthill MO 65488",
+  ssn: "000000015"
 })
