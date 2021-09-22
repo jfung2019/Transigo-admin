319237d006844fc1025882af902fcf8e2b346b78
gabriel <gabriel@transigo.io>
2021-09-22 12:08:08 +0800

    add importer to make testing easier

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -16,7 +16,7 @@ Repo.insert!(%TransigoAdmin.Credit.Marketplace{
   marketplace: "DHGate"
 })
 
-Repo.insert!(%TransigoAdmin.Account.Contact{
+contact = Repo.insert!(%TransigoAdmin.Account.Contact{
   contact_transigo_uid: TransigoAdmin.DataLayer.generate_uid("con"),
   first_name: "Bill",
   last_name: "TestCase",
@@ -28,3 +28,19 @@ Repo.insert!(%TransigoAdmin.Account.Contact{
   address: "8842 48th Ave Anthill MO 65488",
   ssn: "000000015"
 })
+
+Repo.insert!(%TransigoAdmin.Account.Importer{
+    importer_transigo_uid: TransigoAdmin.DataLayer.generate_uid("imp"),
+    business_name: "test",
+    business_ein: "ein",
+    incorporation_date: Timex.today(),
+    number_duns: "duns",
+    business_address_street_address: "100 street",
+    business_address_city: "city",
+    business_address_state: "state",
+    business_address_zip: "00000",
+    business_address_country: "country",
+    business_type: :soleProprietorship,
+    business_classification_id: "9ed46ba1-7d6f-11e3-9d1b-5404a6144203",
+    contact_id: contact.id
+  })
