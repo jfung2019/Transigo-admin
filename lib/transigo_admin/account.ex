ca596ad81bcbacf9a388a2165ec6968e3cd287ce
gabriel <gabriel@transigo.io>
2021-09-24 09:41:59 +0800

    added missing aliases

--- lib/transigo_admin/account.ex
+++ lib/transigo_admin/account.ex
@@ -9,6 +9,7 @@ defmodule TransigoAdmin.Account do
   alias Ecto.Multi
   alias TransigoAdmin.DataLayer
   alias TransigoAdmin.Credit.Marketplace
+  alias TransigoAdmin.Credit.Quota
 
   alias TransigoAdmin.Account.{
     Admin,
