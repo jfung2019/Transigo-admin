92d66c9d623f3b9508a354b9892443bf7fd08e82
gabriel <gabriel@transigo.io>
2021-09-24 10:17:59 +0800

    add side effect checks to test

--- test/transigo_admin/meridianlink/meridianlink_test.exs
+++ test/transigo_admin/meridianlink/meridianlink_test.exs
@@ -10,6 +10,17 @@ defmodule TransigoAdmin.MeridianlinkTest do
       Repo.all(Contact)
       |> List.first()
 
+    # credit fields are nil
+    assert contact.consumer_credit_score == nil
+    assert contact.consumer_credit_score_percentile == nil
+    assert contact.consumer_credit_report_meridianlink == nil
+
     assert :ok = Meridianlink.update_contact_consumer_credit_report(contact.id)
+
+    # after update credit fields are filled in
+    contact = Repo.get!(Contact, contact.id)
+    assert contact.consumer_credit_score != nil
+    assert contact.consumer_credit_score_percentile != nil
+    assert contact.consumer_credit_report_meridianlink != nil
   end
 end
