5ff6bb99ff599cf4f0059e769a83002ee9125d71
raymond leung <raymond@plangora.com>
2022-01-04 11:52:27 +0800

    fix_get_total_open_factoring_price_and_add_test

--- test/transigo_admin/credit_test.exs
+++ test/transigo_admin/credit_test.exs
@@ -1,7 +1,7 @@
 defmodule TransigoAdmin.CreditTest do
   use TransigoAdmin.DataCase, async: false
 
-  alias TransigoAdmin.{Account, Credit}
+  alias TransigoAdmin.{Account, Credit, DataLayer}
   alias TransigoAdmin.Account.Exporter
 
   describe "transaction" do
@@ -289,6 +289,83 @@ defmodule TransigoAdmin.CreditTest do
 
       assert {:error, "offer already accepted or declined"} =
                Credit.accept_decline_offer(uid2, false)
+    end
+
+    test "can get_total_open_factoring_price return correct price", %{
+      exporter: exporter,
+      importer: importer
+    } do
+
+      transaction_uid_list =
+        1..10
+        |> Enum.map(fn _ ->
+          {:ok, %{transaction_uid: tra_uid, id: tx_id}} =
+            Credit.create_transaction(%{
+              transaction_uid: DataLayer.generate_uid("tra"),
+              credit_term_days: 60,
+              down_payment_usd: 3000,
+              factoring_fee_usd: 3000,
+              transaction_state: "created",
+              financed_sum: 3000,
+              invoice_date: Timex.now(),
+              second_installment_usd: 3000,
+              importer_id: importer.id,
+              exporter_id: exporter.id
+            })
+
+          {:ok, %{id: _of_id}} =
+            Credit.create_offer(%{
+              transaction_id: tx_id,
+              transaction_usd: 10000,
+              advance_percentage: 30,
+              advance_usd: 3000,
+              importer_fee: 550,
+              offer_accepted_declined: nil
+            })
+
+          tra_uid
+        end)
+
+      # case with offer_accepted_declined be the first-eighth="A", ninth="D", lastone= nil
+
+      accept_transactoin_id_list = Enum.drop(transaction_uid_list, -2)
+
+      declined_transaction_id_list = [Enum.at(transaction_uid_list, 8)]
+
+      Enum.map(accept_transactoin_id_list, fn tra_uid ->
+        Credit.accept_decline_offer(tra_uid, true)
+      end)
+
+      Enum.map(declined_transaction_id_list, fn tra_uid ->
+        Credit.accept_decline_offer(tra_uid, false)
+      end)
+
+      # case with transaction_state = "created","xxx","repaid","rev_share_paid","disputed","moved_to_payment","originated"
+
+      transaction_state_list = [
+        "repaid",
+        "rev_share_paid",
+        "rev_share_to_be_paid",
+        "disputed",
+        "moved_to_payment",
+        "originated",
+        "down_payment_done",
+        "assigned",
+        "xxx",
+        "created"
+      ]
+
+      # map each uid in list_transaction_id into one state of transaction_state_list
+
+      uid_state_pair_list = Enum.zip(transaction_uid_list, transaction_state_list)
+
+      Enum.map(uid_state_pair_list, fn x ->
+        Credit.update_transaction(Credit.get_transaction_by_transaction_uid(elem(x, 0)), %{
+          transaction_state: elem(x, 1)
+        })
+      end)
+
+      assert Credit.get_total_open_factoring_price(importer.id) == 18000
     end
   end
 end
