cb1fd6882598bc0290b5ad2a5cd38e18d5a0fab8
gabriel <gabriel@transigo.io>
2022-01-12 11:45:45 +0800

    fix file overwrite

--- lib/transigo_admin/job/daily_balance.ex
+++ lib/transigo_admin/job/daily_balance.ex
@@ -45,8 +45,8 @@ defmodule TransigoAdmin.Job.DailyBalance do
     Logger.info("The send_report_result is: is -> #{send_report_result}")
     Logger.error("The send_report_result is: is ->")
     Logger.error(send_report_result)
- 
-    #if email_state return :ok , do notify_webhook, else return :error, break  
+
+    #if email_state return :ok , do notify_webhook, else return :error, break
     case send_report_result do
       :ok -> transactions |> notify_webhook()
       {:error,_} -> send_report_result
